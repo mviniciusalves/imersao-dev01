0525d970eff11bacbda88a3d4ec2845923dd0e17
Marcos Vinicius <marcos.mva18@hotmail.com>
2021-09-16 15:51:34 -0300

    adicionado form

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 var nome = "Marcos" // palavras sempre em aspas
 
-var notaPrimeiroBimestre = 9.45
-var notaSegundoBimestre = 3.123
-var notaTerceiroBimestre = 7.12
-var notaQuartoBimestre = 8.45
+var notaUm = document.getElementById('nota1')
+var notaDois = document.getElementById('nota2')
 
-var mediaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4
-
-var notaFixada = mediaFinal.toFixed(2) // fixar casas decimais
-
-console.log("A média foi: " + notaFixada)
+console.log("A primeira nota foi: " + notaUm)
+console.log("A segunda nota foi: " + notaDois)
